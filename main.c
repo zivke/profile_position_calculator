9e515234404ea59c6966977ecaf53b71be49e9dd
Nikola Zivkovic <zivke85@gmail.com>
2016-06-17 02:11:11 +0200

    - Read position data from the input file - Multiple fixes

--- main.c
+++ main.c
@@ -48,36 +48,48 @@ motion_profile_t * init_position_profile_multi_axis(int *target_positions, int *
 
         init_position_profile_limits(&motion_profiles[i], MAX_ACCELERATION, MAX_VELOCITY, MAX_POSITION, MIN_POSITION);
 
+        // TODO: The case when the target position is smaller than the MAX_ACCELERATION
         while(1) {
             calculated_steps[i] = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], profile_velocity[i], \
                                                         acceleration, deceleration);
             if (calculated_steps[i] > max_steps) {
-                calculated_steps[i] = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], ++profile_velocity[i], \
-                                                            acceleration, deceleration);
+                // Make sure that returning to a previous position works as well
+                int profile_velocity_correction;
+                if (profile_velocity[i] > 0) {
+                    profile_velocity_correction = profile_velocity[i]++;
+                } else {
+                    profile_velocity_correction = profile_velocity[i]--;
+                }
+
+                calculated_steps[i] = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], \
+                                                            profile_velocity_correction, acceleration, deceleration);
                 break;
             }
-            profile_velocity[i]--;
+
+            // Make sure that returning to a previous position works as well
+            if (profile_velocity[i] > 0) {
+                profile_velocity[i]--;
+            } else {
+                profile_velocity[i]++;
+            }
         }
     }
 
     return motion_profiles;
 }
 
-int main () {
-
-    int start_positions[TOTAL_MOTORS] = { 0, 0, 0, 0, 0, 0 };
-    int target_positions[TOTAL_MOTORS] = { 100000, 200000, 300000, 400000, 500000, 600000 };
-
+// Calculate the profile position sections and APPEND them to the result file
+void connect_positions(int *target_positions, int *start_positions, int n_axis, int gripper) {
     motion_profile_t *motion_profiles;
 
-    motion_profiles = init_position_profile_multi_axis(target_positions, start_positions, TOTAL_MOTORS, \
+    motion_profiles = init_position_profile_multi_axis(target_positions, start_positions, n_axis, \
                                                        USER_PROFILE_VELOCITY, USER_PROFILE_ACCELERATION, USER_PROFILE_DECELERATION);
 
     int delta_max = 0;
     int highest_offset_motor_index = 0;
 
     // Find highest position offset motor index
-    for (int i = 0; i < TOTAL_MOTORS; i++) {
+    for (int i = 0; i < n_axis; i++) {
         int delta = target_positions[i] - start_positions[i];
         if (delta > delta_max) {
             delta_max = delta;
@@ -85,22 +97,80 @@ int main () {
         }
     }
 
-    FILE *f = fopen("positions.csv", "w");
-    if (f == NULL)
+    FILE *file = fopen("calculated_positions.csv", "a");
+    if (file == NULL)
     {
-        printf("Error opening file!\n");
+        printf("Error opening output file!\n");
         exit(1);
     }
 
     for (int i = 0; i <= motion_profiles[highest_offset_motor_index].steps; i++) {
-        for (int j =0; j < TOTAL_MOTORS; j++) {
+        for (int j = 0; j < n_axis; j++) {
             int position = position_profile_generate(&motion_profiles[j], i);
-            fprintf(f, "%d,",position);
+            if (j + 1 < n_axis) {
+                fprintf(file, "%d,",position);
+            } else {
+                // Finish the line with the gripper value
+                fprintf(file, "%d,%d\n", position, gripper);
+            }
         }
-        fprintf(f, "0\n");
     }
 
-    fclose(f);
+    fclose(file);
+}
+
+int main(int argc, char **argv) {
+
+    // The first argument is the input file name (CSV format with commas)
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening input file!\n");
+        exit(1);
+    }
+
+    // Count all of the target points in the input file (by counting rows)
+    char c;
+    int n_rows = 0;
+    while ((c = getc(file)) != EOF) {
+        if (c == '\n') {
+            n_rows++;
+        }
+    }
+    // Reset the file pointer to the beginning of the file
+    rewind(file);
+
+    // Create the 2D array to hold all of the read data from the input file
+    int **array = malloc(n_rows * sizeof *array + (n_rows * (TOTAL_MOTORS * sizeof **array)));
+    int *data = array + n_rows;
+    for(int i = 0; i < n_rows; i++) {
+        array[i] = data + i * TOTAL_MOTORS;
+    }
+
+    int gripper = 0;
+
+    // Read each line of data and put in its place (can read exactly 6 positions and the gripper value)
+    int line = 0;
+    while(!feof(file)) {
+        fscanf(file, "%d,%d,%d,%d,%d,%d,%d\n", &array[line][0], &array[line][1], &array[line][2], &array[line][3], \
+                                               &array[line][4], &array[line][5], &gripper);
+        line++;
+    }
+
+    fclose(file);
+
+    file = fopen("calculated_positions.csv", "w");
+    if (file == NULL)
+    {
+        printf("Error opening output file!\n");
+        exit(1);
+    }
+
+    // Calculate the profile position sections and APPEND them to the result file
+    for (int i = 0; i < n_rows; i++) {
+        connect_positions(array[i+1], array[i], TOTAL_MOTORS, gripper);
+    }
+
+    fclose(file);
 
     return 0;
 }

0dfb6b6a03f98828a9e08c3c6d746696e2c76029
Nikola Zivkovic <zivke85@gmail.com>
2016-06-17 12:00:12 +0200

    Simplify and reformat

--- main.c
+++ main.c
@@ -12,17 +12,10 @@
 #define USER_PROFILE_ACCELERATION 4000
 #define USER_PROFILE_DECELERATION 4000
 
-motion_profile_t * init_position_profile_multi_axis(int *target_positions, int *start_positions, int n_axis, int velocity, int acceleration, int deceleration) {
-
-    motion_profile_t *motion_profiles = malloc(n_axis * sizeof(motion_profile_t));
-
-    int *calculated_steps = malloc(n_axis * sizeof(int));
-    int *profile_velocity = malloc(n_axis * sizeof(int));
-
+// Find highest position offset motor index
+int get_highest_position_offset_motor(int *target_positions, int *start_positions, int n_axis) {
     int delta_max = 0;
     int highest_offset_motor_index = 0;
-
-    // Find highest position offset motor index
     for (int i = 0; i < n_axis; i++) {
         int delta = target_positions[i] - start_positions[i];
         if (delta > delta_max) {
@@ -31,52 +24,59 @@ motion_profile_t * init_position_profile_multi_axis(int *target_positions, int *
         }
     }
 
+    return highest_offset_motor_index;
+}
+
+motion_profile_t * init_position_profile_multi_axis(int *target_positions, int *start_positions, int n_axis, \
+                                                    int velocity, int acceleration, int deceleration) {
+
+    motion_profile_t *motion_profiles = malloc(n_axis * sizeof(motion_profile_t));
+
+    int highest_offset_motor_index = get_highest_position_offset_motor(target_positions, start_positions, n_axis);
+
     init_position_profile_limits(&motion_profiles[highest_offset_motor_index], MAX_ACCELERATION, MAX_VELOCITY, MAX_POSITION, MIN_POSITION);
 
-    int max_steps = init_position_profile(&motion_profiles[highest_offset_motor_index], target_positions[highest_offset_motor_index], start_positions[highest_offset_motor_index], \
-                                          velocity, acceleration, deceleration);
+    int max_steps = init_position_profile(&motion_profiles[highest_offset_motor_index], target_positions[highest_offset_motor_index], \
+                                          start_positions[highest_offset_motor_index], velocity, acceleration, deceleration);
 
     for (int i = 0; i < n_axis; i++) {
         if (highest_offset_motor_index == i) {
-            profile_velocity[i] = velocity;
-            calculated_steps[i] = max_steps;
             continue;
         }
 
-        calculated_steps[i] = 0;
-        profile_velocity[i] = velocity;
+        int calculated_steps = 0;
+        int profile_velocity = velocity;
 
         init_position_profile_limits(&motion_profiles[i], MAX_ACCELERATION, MAX_VELOCITY, MAX_POSITION, MIN_POSITION);
 
         // TODO: The case when the target position is smaller than the MAX_ACCELERATION
         while(1) {
-            calculated_steps[i] = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], profile_velocity[i], \
-                                                        acceleration, deceleration);
-            if (calculated_steps[i] > max_steps) {
+            calculated_steps = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], \
+                                                     profile_velocity, acceleration, deceleration);
+            if (calculated_steps > max_steps) {
                 // Make sure that returning to a previous position works as well
                 int profile_velocity_correction;
-                if (profile_velocity[i] > 0) {
-                    profile_velocity_correction = profile_velocity[i]++;
+                if (profile_velocity > 0) {
+                    profile_velocity_correction = profile_velocity++;
                 } else {
-                    profile_velocity_correction = profile_velocity[i]--;
+                    profile_velocity_correction = profile_velocity--;
                 }
 
-                calculated_steps[i] = init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], \
-                                                            profile_velocity_correction, acceleration, deceleration);
+                // Just recalculate the position profile with the corrected velocity to get the correct motion profile
+                // (no need to actually save the calculated steps)
+                init_position_profile(&motion_profiles[i], target_positions[i], start_positions[i], \
+                                      profile_velocity_correction, acceleration, deceleration);
                 break;
             }
 
             // Make sure that returning to a previous position works as well
-            if (profile_velocity[i] > 0) {
-                profile_velocity[i]--;
+            if (profile_velocity > 0) {
+                profile_velocity--;
             } else {
-                profile_velocity[i]++;
+                profile_velocity++;
             }
         }
     }
-
-    free(calculated_steps);
-    free(profile_velocity);
 
     return motion_profiles;
 }
@@ -88,17 +88,7 @@ void connect_positions(int *target_positions, int *start_positions, int n_axis, 
     motion_profiles = init_position_profile_multi_axis(target_positions, start_positions, n_axis, \
                                                        USER_PROFILE_VELOCITY, USER_PROFILE_ACCELERATION, USER_PROFILE_DECELERATION);
 
-    int delta_max = 0;
-    int highest_offset_motor_index = 0;
-
-    // Find highest position offset motor index
-    for (int i = 0; i < n_axis; i++) {
-        int delta = target_positions[i] - start_positions[i];
-        if (delta > delta_max) {
-            delta_max = delta;
-            highest_offset_motor_index = i;
-        }
-    }
+    int highest_offset_motor_index = get_highest_position_offset_motor(target_positions, start_positions, n_axis);
 
     // Open the file for appending
     FILE *file = fopen("calculated_positions.csv", "a");
